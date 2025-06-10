846ba83915a38a205ec3ea01f0648d1ae107139c
Loris Leopold Kulke <kulke.loris.leopold@gmail.com>
2025-06-11 01:41:26 +0200

    Added JSON mapping and comments

--- src/app/(tabs)/scan/camera.tsx
+++ src/app/(tabs)/scan/camera.tsx
@@ -1,37 +1,130 @@
+import { GEMINI_API_KEY } from './GEMINI_API_KEY.js';
+
 import { useTranslation } from "react-i18next"
 import { TouchableOpacity, Dimensions, View, Text } from "react-native"
-
 import { router, useFocusEffect } from "expo-router"
-
 import { useState, useRef } from "react"
 import { CameraType, useCameraPermissions, CameraView } from "expo-camera"
 import { useIsFocused } from '@react-navigation/native';
 
-import { GEMINI_API_KEY } from './GEMINI_API_KEY.js';
 
 
 export default function CameraScreen() 
 {
+  // Variables
+  // * * * * * * * * * * * * 
+
+  // Translation
   const { t } = useTranslation();
 
-  // back => back camera  
+  // Camera variables
   const facing = "back" as CameraType;
-  // saves permission for camera usage 
+  const cameraRef = useRef(null);
   const [permission, requestPermission] = useCameraPermissions();
-  // Used to deactivade CameraView on tab change
+  
+  // View variables 
   const isFocused = useIsFocused();
   const windowWidth = Dimensions.get("window").width;
-  const cameraRef = useRef(null);
-
-
   const [isProcessing, changeState] = useState<Boolean>(false);
+  
 
+  // Model
+  // * * * * * * * * * * * * 
 
-  // Check if permission is given
-  if(!permission?.granted)
+  async function takePicture()
+  {
+    if(cameraRef.current) 
+    {
+      const photo = await cameraRef.current.takePictureAsync({ base64: true });
+      askGemini(photo);
+    }  
+  }
+
+  /*
+    Function to open galery
+  */
+
+  async function askGemini(photo) 
+  {
+    const apiKey = GEMINI_API_KEY;
+    const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
+
+    /*
+      HERE WE EDIT THE PROMPTS AND THE MODEL 
+    */
+
+    // Just for testing at the moment, needs data from database and refacotor
+    const textInput = "Gib mir eine JSON-Antwort zurück. Erkenne in diesem Bild alle Artikel mit ihrem Preis und einem erkannten Namen (name_scan). Bei Ausgaben soll der Betrag negativ in der Ausgabe sein. Alle Einnahmen und Rabatte sollen unter Einnahmen kategorisiert werden und positiv in der Ausgabe sein." +
+      "Leite aus name_scan eine allgemeinere Produktbezeichnung ab (name). Im Anhang sind alle schon existierenden Produktbezeichnungen, an denen kannst du dich orintieren. Ordne jeden Artikel genau einer Kategorie zu, sei Kreativ und ordne es möglichst tief im Baum ein . " +
+      "Verwende dabei nur die Kategorie-ID aus der folgenden Liste. Verwende nur die Währungs ids aus der folgenden Liste. Gib für jeden Artikel ein JSON-Objekt mit diesen Feldern zurück: " +
+      "- name_scan (Kurzbezeichnung aus dem Bild) " +
+      "- name (allgemeinere Produktbezeichnung) " +
+      "- amount (Preis/Betrag in Euro) " +
+      "- category_id (ID der zugewiesenen Kategorie)" +
+      "- original_currency_id (ursprüngliche Währung)" +
+      "- original_amount (ursprünglicher Preis/Betrag in der original Währung, Ausgaben sollen negativ sein, Einnahmen positiv)" +
+      "- quantity (Menge in Anzahl, Gewicht oder Volumen mit Einheit angegeben)";
+    
+    changeState(true);
+
+    try 
+    {
+      const response = await fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                { text: textInput },
+                {
+                  inlineData: {
+                    mimeType: photo.base64 ? 'image/jpeg' : 'application/octet-stream',
+                    data: photo.base64
+                  }
+                }
+              ]
+            }
+          ]
+        })
+      });
+
+      if (!response.ok) 
+      {
+        const errorData = await response.text();
+        throw new Error(`HTTP error! status: ${response.status}, message: ${errorData}`);
+      }
+
+      const data = await response.json();
+      changeState(false);
+      router.push({pathname: "/scan/input", params: {geminiResponse: data.candidates[0].content.parts[0].text}});
+    } 
+    catch (error) 
+    {
+      console.error("Error sending picture to Gemini API:", error);
+    }
+
+    changeState(false);
+  }
+ 
+
+  // Main
+  // * * * * * * * * * * * * 
+
+  if(!permission?.granted) // Check if camera permission is given
   {
     requestPermission();
-    // No permission given
+  }
+
+
+  // View
+  // * * * * * * * * * * * * 
+
+  if(!permission?.granted) // Check if camera permission is given
+  {
+    // No camery permission given
     return(
       <View className='flex-1 items-center justify-center bg-background'>
         <Text>
@@ -40,82 +133,13 @@ export default function CameraScreen()
       </View>
     );
   }
-
-
-
-
-  // Logic
-
-  async function takePicture() 
-  {
-    if(cameraRef.current) 
-    {
-      const photo = await cameraRef.current.takePictureAsync({ base64: true });
-      const apiKey = GEMINI_API_KEY;
-      const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
-
-      const textInput = "Gib mir eine JSON-Antwort zurück. Erkenne in diesem Bild alle Artikel mit ihrem Preis und einem erkannten Namen (name_scan). Bei Ausgaben soll der Betrag negativ in der Ausgabe sein. Alle Einnahmen und Rabatte sollen unter Einnahmen kategorisiert werden und positiv in der Ausgabe sein." +
-        "Leite aus name_scan eine allgemeinere Produktbezeichnung ab (name). Im Anhang sind alle schon existierenden Produktbezeichnungen, an denen kannst du dich orintieren. Ordne jeden Artikel genau einer Kategorie zu, sei Kreativ und ordne es möglichst tief im Baum ein . " +
-        "Verwende dabei nur die Kategorie-ID aus der folgenden Liste. Verwende nur die Währungs ids aus der folgenden Liste. Gib für jeden Artikel ein JSON-Objekt mit diesen Feldern zurück: " +
-        "- name_scan (Kurzbezeichnung aus dem Bild) " +
-        "- name (allgemeinere Produktbezeichnung) " +
-        "- amount (Preis/Betrag in Euro) " +
-        "- category_id (ID der zugewiesenen Kategorie)" +
-        "- original_currency_id (ursprüngliche Währung)" +
-        "- original_amount (ursprünglicher Preis/Betrag in der original Währung, Ausgaben sollen negativ sein, Einnahmen positiv)" +
-        "- quantity (Menge in Anzahl, Gewicht oder Volumen mit Einheit angegeben)";
-      
-      changeState(true);
-
-      try {
-        const response = await fetch(endpoint, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            contents: [
-              {
-                parts: [
-                  { text: textInput },
-                  {
-                    inlineData: {
-                      mimeType: photo.base64 ? 'image/jpeg' : 'application/octet-stream',
-                      data: photo.base64
-                    }
-                  }
-                ]
-              }
-            ]
-          })
-        });
-
-        if (!response.ok) 
-        {
-          const errorData = await response.text();
-          throw new Error(`HTTP error! status: ${response.status}, message: ${errorData}`);
-        }
-
-        const data = await response.json();
-        console.log(data.candidates[0].content.parts[0].text);
-        changeState(false);
-        router.push({pathname: "/scan/input", params: {geminiResponse: data.candidates[0].content.parts[0].text}});
-      } catch (error) {
-        console.error("Error sending picture to Gemini API:", error);
-      }
-    }
-
-    changeState(false);
-  }
- 
-
-  if(!isProcessing)
+  else if(!isProcessing) // Camera View
   {
     return (
       <View className='flex-1 items-center justify-center bg-background'>
         { isFocused?(
         <View style={{width: windowWidth, flex: 1, justifyContent: "center"}}>
-          <CameraView facing={facing} style={{flex: 1}} ref={cameraRef}/>
+          <CameraView shutterSound={false} facing={facing} style={{flex: 1}} ref={cameraRef}/>
         </View>
         ) : null }
 
@@ -124,7 +148,7 @@ export default function CameraScreen()
       </View>
     );
   }
-  else
+  else // Process View
   {
     return(
     <View className='flex-1 items-center justify-center bg-background'>
--- src/app/(tabs)/scan/input.tsx
+++ src/app/(tabs)/scan/input.tsx
@@ -1,15 +1,55 @@
 import { useTranslation } from "react-i18next"
 import { View, Text } from "react-native"
 import { useRoute } from '@react-navigation/native';
+import { useState, useEffect } from "react"
 
 
 export default function InputScreen() 
 {
+  // Variables
+  // * * * * * * * * * * * * 
+
   const { t } = useTranslation();
   const route = useRoute();
-  const { geminiResponse = "" } = route.params || {};
+  let { geminiResponse = "" } = route.params || {};
+  const [transactions, changeTransactions] = useState({});
 
-  
+
+  // Model
+  // * * * * * * * * * * * * 
+
+  function processGeminiResponse()
+  {
+    if(geminiResponse != "")
+    {
+      console.log("PROCSESSING");
+      const cleaned = geminiResponse.replace(/`/g, "").replace("json", "");
+      const parsed = JSON.parse(cleaned);
+      const mapped = parsed.map(item => ({
+        "name": item.name,
+        "term": item.term,
+        "amount": item.amount,
+        "currency": item.currency
+      }));
+
+      changeTransactions(mapped);
+      console.log(mapped);
+    }
+  }
+
+
+  // Main
+  // * * * * * * * * * * * * 
+
+  useEffect(() => {
+    console.log("EFFECT");
+    processGeminiResponse();
+  }, [geminiResponse]);
+
+
+  // View
+  // * * * * * * * * * * * * 
+
   return (
     <View className='flex-1 items-center justify-center bg-background'>
         <Text>{geminiResponse?geminiResponse : ""}</Text>
