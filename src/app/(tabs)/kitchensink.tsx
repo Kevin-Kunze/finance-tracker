0dfce38baf50299d0f0ba6210c22a78360ce08b9
Luca <Lucafunk2002@googlemail.com>
2025-06-15 16:16:19 +0200

    wip: Balance Widget

--- src/app/(tabs)/kitchensink.tsx
+++ src/app/(tabs)/kitchensink.tsx
@@ -1,5 +1,6 @@
 import CircularButton from "@/components/CircularButton"
 import ScreenTitle from "@/components/ScreenTitle"
+import BalanceCard from "@/components/BalanceWidget"
 import { useRouter } from "expo-router"
 import { Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
@@ -16,6 +17,17 @@ export default function KitchensinkScreen() {
         </Text>
         <CircularButton icon='home' onPress={() => router.replace("/home")} />
       </View>
+      
+      <View className='mx-6 flex-col gap-2 mt-4'>
+      <Text className='text-gray-900 dark:text-gray-100'>
+          BalanceWidget
+        </Text>
+      <BalanceCard
+        amount="3.161,45"
+        label="aktueller Kontostand"
+        data={[3800, 3200, 10000, 2900, 3161]} // Beispielverlauf
+      />
+    </View>
     </SafeAreaView>
   )
 }
