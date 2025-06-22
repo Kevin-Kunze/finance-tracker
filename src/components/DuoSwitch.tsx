43101b953dc72adbd6c6f59824473b5b3a60f18a
Kevin-Kunze <kevke.kunze@gmail.com>
2025-06-22 21:20:59 +0200

    fix: duo switch

--- src/components/DuoSwitch.tsx
+++ src/components/DuoSwitch.tsx
@@ -5,34 +5,33 @@ export default function DuoSwitch<T extends string>({
   onChange,
   options,
 }: {
-  value: T
+  value: boolean
   onChange: (val: T) => void
   options: T[]
 }) {
   return (
-    <View className="flex-row rounded-lg overflow-hidden border border-primary-600 dark:border-primary-800 self-center">
-      {options.map((option) => {
-        const isSelected = value === option
+    <View className='flex-row rounded-lg overflow-hidden border border-primary-600 dark:border-primary-800 self-center'>
+      {options.map((option, i) => {
+        const isSelected = value === !!i
         return (
           <TouchableOpacity
-  key={option}
-  onPress={() => onChange(option)}
-  className={`px-4 py-2 ${
-    isSelected
-      ? "bg-primary-600"
-      : "bg-white"
-  }`}
->
-  <Text
-    className={`font-semibold ${
-      isSelected
-        ? "text-white"
-        : "text-primary-600"
-    }`}
-  >
-    {option}
-  </Text>
-</TouchableOpacity>
+            key={option}
+            disabled={isSelected}
+            onPress={() => {
+              onChange(option)
+            }}
+            className={`px-4 py-2 ${
+              isSelected ? "bg-primary-600" : "bg-gray-50"
+            }`}
+          >
+            <Text
+              className={`font-semibold ${
+                isSelected ? "text-gray-50" : "text-primary-600"
+              }`}
+            >
+              {option}
+            </Text>
+          </TouchableOpacity>
         )
       })}
     </View>
--- src/language/translationKeys.ts
+++ src/language/translationKeys.ts
@@ -10,8 +10,10 @@ export type TranslationKey =
   | "screens.settings.title"
   | "screens.settings.general"
   | "screens.settings.categorySettings"
+  | "screens.settings.personalization"
   | "screens.settings.appearance"
-  | "screens.settings.darkMode"
+  | "screens.settings.dark"
+  | "screens.settings.light"
   | "screens.settings.language"
   | "screens.settings.english"
   | "screens.settings.german";
